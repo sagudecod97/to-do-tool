5875996be4ef2f04ed0af70750068bd80a8799b9
Santiago <sagudecod97@gmail.com>
2021-10-21 12:06:53 -0500

    Added mobile styles

--- src/components/molecules/add-todo/addToDo.jsx
+++ src/components/molecules/add-todo/addToDo.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 import './addToDo.scss';
 
+import GitHubLogo from '../../../assets/images/iconmonstr-github-1.svg';
+
 const AddToDo = ({ children }) => {
     return(
         <footer className='add-todo'>
+            <div className='add-todo__info'>
+                <p className='add-todo__name'>Santiago Agudelo Alvarez</p>
+
+                <a
+                href='https://github.com/sagudecod97/to-do-tool'
+                className='add-todo__container'
+                target='_blank'
+                rel="noreferrer"
+                >
+                    <img src={GitHubLogo} alt="Santiago's GitHub" />
+                </a>
+            </div>
             {
                 children
             }
